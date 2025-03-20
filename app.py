8b1c2dd1803d6a6248acf3ebf4ff2d576ac6aea6
MAITRI <161553973+Xyz1255@users.noreply.github.com.>
2025-03-20 15:13:48 +0530

    heart

--- app.py
+++ app.py
@@ -185,10 +185,10 @@ if selected == 'Heart Disease Prediction':
     if st.button('Heart Disease Test Result'):
         if 'heart_disease' not in st.session_state.models or st.session_state.models['heart_disease'] is None:
             st.error("Heart disease model is not available!")
-    else:
-        heart_prediction = st.session_state.models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
-        heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
-        st.success(heart_diagnosis)
+        else:
+            heart_prediction = st.session_state.models['heart_disease'].predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+            heart_diagnosis = 'The person has heart disease' if heart_prediction[0] == 1 else 'The person does not have heart disease'
+            st.success(heart_diagnosis)
 
 # Parkinson's Prediction Page
 if selected == "Parkinsons Prediction":
